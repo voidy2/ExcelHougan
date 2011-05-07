df62a0e51d4dc5580dd3ce00e863a90913222d6f
voidy21 <y2.void+github@gmail.com>
2011-05-07 16:29:33 +0900

    argsやめた

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,7 +2,7 @@ package main.scala
 
 object Main {
   def main(args:Array[String]):Unit = {
-    new ExcelImage(new Config(args(0))).makeExcel
+    new ExcelImage(new Config("config.yml")).makeExcel
   }
 }
 
